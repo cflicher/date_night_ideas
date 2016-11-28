892658c92f03bad190d766aa0b234621426c10bc
Raghu Betina <raghu@firstdraft.com>
2016-11-28 01:29:46 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "photos#index"
   # Routes for the Helpful_rating resource:
   # CREATE
   get "/helpful_ratings/new", :controller => "helpful_ratings", :action => "new"
