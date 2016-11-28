2512b214631529167971e3f1255249fb5cf0537f
Raghu Betina <raghu@firstdraft.com>
2016-11-28 01:30:13 +0000

    Direct association between neighborhoods and date_ideas

--- app/models/date_idea.rb
+++ app/models/date_idea.rb
@@ -1,6 +1,8 @@
 class DateIdea < ApplicationRecord
   # Direct associations
 
+  belongs_to :neighborhood
+
   has_many   :reviews,
              :dependent => :destroy
 
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,6 +1,9 @@
 class Neighborhood < ApplicationRecord
   # Direct associations
 
+  has_many   :date_ideas,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
