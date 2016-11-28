c5aa6e370f5ba930933684d80ae5f31f7727a378
Raghu Betina <raghu@firstdraft.com>
2016-11-28 01:29:40 +0000

    Generate neighborhoods resource with starter:resource

---
+++ db/migrate/20161128012940_create_neighborhoods.rb
@@ -0,0 +1,11 @@
+class CreateNeighborhoods < ActiveRecord::Migration
+  def change
+    create_table :neighborhoods do |t|
+      t.integer :city_id
+      t.string :neighborhood
+
+      t.timestamps
+
+    end
+  end
+end
